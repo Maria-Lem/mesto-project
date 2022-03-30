709d6d11e79005bd215ec84c9e75a52d9a777798
Maria Alemaskina <maria131621@yandex.ru>
2022-03-30 21:10:33 +0300

    feat: add 'adding new card' functionality

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ const popupAdd = document.querySelector('.popup_type_add');
 const buttonEdit = document.querySelector('.button_type_edit');
 const buttonClose = document.querySelectorAll('.button_type_close');
 const buttonAdd = document.querySelector('.button_type_add');
+const buttonCreate = document.querySelector('.button-text_type_add');
 
-const form = document.querySelector('.form');
+const formAdd = document.querySelector('.form_type_add');
 const userName = document.querySelector('.form__input_type_name');
 const userOccupation = document.querySelector('.form__input_type_occupation');
 const cardName = document.querySelector('.form__input_type_place');
@@ -14,27 +15,17 @@ const cardPhoto = document.querySelector('.form__input_type_photo');
 const profileName = document.querySelector('.profile__name');
 const profileOccupation = document.querySelector('.profile__occupation');
 
-function openPopupEdit(e) {
-  if (e.target.matches('.button_type_edit')) {
-    popupEdit.classList.add('popup_opened');
-  } else if (e.target.matches('.button_type_close')) {
-    popupEdit.classList.remove('popup_opened');
-  }
-  // if (popupEdit.classList.contains('popup_opened') === true) {
-  //   popupEdit.classList.remove('popup_opened');
-  // } else {
-  //   popupEdit.classList.add('popup_opened');
-  // }
-}
-console.log(buttonClose);
-function openPopupAdd() {
-  if (popupAdd.classList.contains('popup_opened') === true) {
-    popupAdd.classList.remove('popup_opened');
-  } else {
-    popupAdd.classList.add('popup_opened');
-  }
-}
+const cardsBlock = document.querySelector('.cards');
 
+// function openPopupEdit(e) {
+//   if (e.target.matches('.button_type_edit')) {
+//     popupEdit.classList.add('popup_opened');
+//   } else if (e.target.matches('.button_type_close')) {
+//     popupEdit.classList.remove('popup_opened');
+//   }
+// }
+
+// Open and close modal wondows
 function openEdit() {
   popupEdit.classList.add('popup_opened');
 }
@@ -51,13 +42,42 @@ function closeAdd() {
   popupAdd.classList.remove('popup_opened');
 }
 
+function addCard(placeValue, photoValue) {
+  const cardContainer = document.createElement('article');
+  cardContainer.classList.add('card');
+
+  const cardImage = document.createElement('img');
+  cardImage.classList.add('card__image');
+  cardImage.src = photoValue;
+  cardImage.alt = placeValue;
+
+  const cardContent = document.createElement('div');
+  cardContent.classList.add('card__content');
+
+  const cardTitle = document.createElement('h3');
+  cardTitle.classList.add('card__title');
+  cardTitle.textContent = placeValue;
+
+  const cardButton = document.createElement('button');
+  cardButton.classList.add('button', 'button_type_like');
+
+  cardContent.append(cardTitle, cardButton);
+  cardContainer.append(cardImage, cardContent);
+  cardsBlock.prepend(cardContainer);
+}
+
 // При открытии модального окна, шначения инпутов автоматически заполняются значениями в профиле
 userName.value = profileName.textContent;
 userOccupation.value = profileOccupation.textContent;
 
 buttonAdd.addEventListener('click', openAdd);
 buttonClose[1].addEventListener('click', closeAdd);
-// buttonEdit.addEventListener('click', openPopupEdit);
-// buttonClose.addEventListener('click', openPopupEdit);
 buttonEdit.addEventListener('click', openEdit);
 buttonClose[0].addEventListener('click', closeEdit);
+formAdd.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log('Форма отправлена');
+  addCard(cardName.value, cardPhoto.value);
+  formAdd.reset();
+  closeAdd();
+});
